604c55d7dea3a743b8cbab04dac124fdf7cd9aec
shohagroy <pkshohag240@gmail.com>
2022-08-19 23:01:20 +0600

    project complited

--- js/apps.js
+++ js/apps.js
@@ -22,17 +22,47 @@ document.getElementById('calculator').addEventListener('click',(event) => {
             typedNumberdisplayFild.value = "";
         }
         else if(typedNumber == "<"){
-            console.log('< area deleted')
+
+            const typedNumberArray = privauseTypedNumber.split('');
+            typedNumberArray.pop();
+
+            const reaminNumber = typedNumberArray.join('');
+            typedNumberdisplayFild.value = reaminNumber
         }
-        
-        
     }
     else{
         const newTypedNumber = typedNumberdisplayFild.value = privauseTypedNumber + typedNumber;;
         console.log(newTypedNumber)
     }
+})
 
+// submit button clicket function 
+document.getElementById('verify-pin').addEventListener('click', ()=>{
+    const genaratPinNumberFild = document.getElementById('display-pin');
+    const genaratPinNumber = genaratPinNumberFild.value;
+    
+    const typedPinNumberFild = document.getElementById('typed-numbers')
+    const typedPinNumber = typedPinNumberFild.value;
+
+    if(genaratPinNumber === "" || typedPinNumber === ""){
+        alert("Please use OTP and Varify this.!")
+    }
+    else{
+        console.log("other area")
+        if(genaratPinNumber === typedPinNumber ){
+            document.getElementById('pin-success').style.display = "block";
+            document.getElementById('pin-failure').style.display = "none";
+        }
+        else{
+            document.getElementById('pin-success').style.display = "none";
+            document.getElementById('pin-failure').style.display = "block";
+        }
+    }
     
 
+    genaratPinNumberFild.value = "";
+    typedPinNumberFild.value = "";
+
+    
 
 })
