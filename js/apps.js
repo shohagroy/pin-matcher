0671cfdc9db941b8d1754ad46f3fe3c559ab93f2
shohagroy <pkshohag240@gmail.com>
2022-08-19 22:19:03 +0600

    calculator function added

--- js/apps.js
+++ js/apps.js
@@ -9,7 +9,30 @@ document.getElementById('generate-pin').addEventListener('click',  () =>{
 }) 
 
 // mobile button set function 
-document.getElementById('calculator').addEventListener('click',() => {
-    console.log('hello')
+document.getElementById('calculator').addEventListener('click',(event) => {
+    
+    const typedNumber = event.target.innerText;
+
+    const typedNumberdisplayFild = document.getElementById('typed-numbers');
+    const privauseTypedNumber = typedNumberdisplayFild.value;
+    
+    if(isNaN(typedNumber)){
+
+        if(typedNumber == 'C'){
+            typedNumberdisplayFild.value = "";
+        }
+        else if(typedNumber == "<"){
+            console.log('< area deleted')
+        }
+        
+        
+    }
+    else{
+        const newTypedNumber = typedNumberdisplayFild.value = privauseTypedNumber + typedNumber;;
+        console.log(newTypedNumber)
+    }
+
+    
+
 
 })
